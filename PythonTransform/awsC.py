996494f144a8f1b785abde2176ed7243220ee1a9
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-08-27 20:18:39 -0400

    Updated boto3 and Spark connection

--- PythonTransform/awsC.py
+++ PythonTransform/awsC.py
@@ -1,6 +1,9 @@
 import time
 import boto3
 import creds
+import sparkC
+import pandas as pd
+from io import StringIO
 
 start_time = time.time()
 
@@ -9,28 +12,31 @@ session = boto3.Session(
     aws_secret_access_key=creds.secretkey
 )
 
+spark = sparkC.spark   #Loads and runs sparkC file (imported first)
+
 s3 = session.resource('s3')
 
-"""
-for bucket in s3.buckets.all():
-    print(bucket.name)
-
-
-
-bucket = s3.Bucket('jtechuspopulationp3')
-for obj in bucket.objects.all():
-    key = obj.key
-    body = obj.get()['Body'].read()
-    
-    print(key)
-    print(body)
-    print(str(obj))
-"""
-
-
-#s3 = boto3.resource('s3')
-obj = s3.Object("jtechuspopulationp3", "predata/Combine2000RG.csv")
+obj = s3.Object("jtechuspopulationp3", "predata/Combine2010RG.csv")
 body = obj.get()['Body'].read()
-print(body)
+#print(body)
+
+# ---------------------------------------------------------------------------------------------
+from smart_open import smart_open
+
+path = 's3://{}:{}@{}/{}'.format(creds.accesskey, creds.secretkey, "jtechuspopulationp3", "predata/Combine2010RG.csv")
+
+df0 = pd.read_csv(smart_open(path))
+print(df0)
+
+df1=spark.createDataFrame(df0) 
+df1.show()
+
+df2 = df1.select("STUSAB","P0010001")
+
+df2.createOrReplaceTempView("TestT2") 
+
+spark.sql("SELECT STUSAB FROM TestT2").show()
 
 print("--- %s seconds ---" % round((time.time() - start_time),4))
+
+#Current thread: https://stackoverflow.com/questions/38867472/spark-select-where-or-filtering
--- PythonTransform/sparkC.py
+++ PythonTransform/sparkC.py
@@ -2,10 +2,15 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 import time
+import creds
 
 start_time = time.time()
 
-spark = SparkSession.builder.master("local").appName("Testing").config("spark.some.config.option", "some-value").getOrCreate()
+spark = SparkSession.builder.master("local").appName("Testing") \
+.config('fs.s3n.awsAccessKeyId', creds.accesskey) \
+.config('fs.s3n.awsSecretAccessKey', creds.secretkey) \
+.config("spark.some.config.option", "some-value") \
+.getOrCreate()
 
 df0 = spark.read.csv('data/Combine2000RG.csv',header=True)
 
@@ -18,6 +23,10 @@ df0.createOrReplaceTempView("TestT")
 
 spark.sql("SELECT STUSAB FROM TestT").show()
 
+
+
+#df = spark.read.load('s3://jtechuspopulationp3/predata/Combine2000RG.csv')
+#df.show()
 
 print("--- %s seconds ---" % round((time.time() - start_time),4))
 
