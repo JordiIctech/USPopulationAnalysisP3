d65bbb2f9effb65a61e300c1ce69c20ae683b79c
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-09-13 21:06:42 -0400

    Added list of states

--- PythonTransform/ETL+/ETL+.py
+++ PythonTransform/ETL+/ETL+.py
@@ -1,3 +1,63 @@
 #Beautiful soup
 #python data scraping
 #python download file from url
+#https://www.census.gov/data/datasets/2000/dec/redistricting.html
+
+
+states = [["Alabama", "al"], 
+      ["Alaska", "ak"],    
+      ["Arizona", "az"],
+      ["Arkansas", "ar"],
+      ["California", "ca"],
+      ["Colorado", "co"],
+      ["Connecticut", "ct"],
+      ["Delaware", "de"],
+      ["District_of_Columbia", "dc"],
+      ["Florida", "fl"],
+      ["Georgia", "ga"],
+      ["Hawaii", "hi"],
+      ["Idaho", "id"],
+      ["Illinois", "il"],
+      ["Indiana", "in"],
+      ["Iowa", "ia"],
+      ["Kansas", "ks"],
+      ["Kentucky", "ky"],
+      ["Louisiana", "la"],
+      ["Maine", "me"],
+      ["Maryland", "md"],
+      ["Massachusetts", "ma"],
+      ["Michigan", "mi"],
+      ["Minnesota", "mn"],
+      ["Mississippi", "ms"],
+      ["Missouri", "mo"],
+      ["Montana", "mt"],
+      ["Nebraska", "ne"],
+      ["Nevada", "nv"],
+      ["New_Hampshire", "nh"],
+      ["New_Jersey", "nj"],
+      ["New_Mexico", "nm"],
+      ["New_York", "ny"],
+      ["North_Carolina", "nc"],
+      ["North_Dakota", "nd"],
+      ["Ohio", "oh"],
+      ["Oklahoma", "ok"],
+      ["Oregon", "or"],
+      ["Pennsylvania", "pa"],
+      ["Puerto_Rico", "pr"],
+      ["Rhode_Island", "ri"],
+      ["South_Carolina", "sc"],
+      ["South_Dakota", "sd"],
+      ["Tennessee", "tn"],
+      ["Texas", "tx"],
+      ["Utah", "ut"],
+      ["Vermont", "vt"],
+      ["Virginia", "va"],
+      ["Washington", "wa"],
+      ["West_Virginia", "wv"],
+      ["Wisconsin", "wi"],
+      ["Wyoming", "wy"]]
+
+
+for i in states:
+    for z in i:
+        print(z)
