f95016096ee20a203c75b3fc084be6c94b28c489
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-09-20 06:16:42 -0400

    Year 2000 filtering

--- PythonTransform/queries+.py
+++ PythonTransform/queries+.py
@@ -29,6 +29,22 @@ AND NOT (NAME LIKE '%CDP%') AND POP100 > 100000")
 
 geodatahead.show()
 
+#---------------------------------------------------------------------------------
+divs = [2, 3, 2, 3, 2, 7, 1, 1, 2, 2, 3, 2, 5, 2, 2, 5, 2, 1, 2, 6, 1, 4, 2, 5, 2, \
+2, 4, 5, 2, 1, 3, 5, 2, 5, 2, 4, 2, 2, 1, 4, 4, 1, 2, 1, 5, 2, 1, 1, 2, 2, 2, 2, 3, \
+3, 6, 1, 3, 5, 2, 14, 14, 90, 1, 1, 9, 9, 9, 10, 2, 1, 5, 5, 5, 6, 5, 5, 5, 32]
+df2 = spark.read.option("delimiter", "  ").csv("data/geo2000/akgeo.upl")
+df2.show()
+
+geodfhead2 = df2.toDF(*GeoNames)
+geodfhead2.createOrReplaceTempView("GeoHeaderakImp2")
+
+geodatahead2 = spark.sql("SELECT STUSAB, SUMLEV, LOGRECNO, GEOID, BASENAME, NAME, POP100, INTPTLAT, INTPTLON \
+FROM GeoHeaderakImp2 WHERE SUMLEV = 160 AND NOT (NAME LIKE '%CCD%') AND NOT (NAME LIKE '%township%') AND NOT (NAME LIKE '%(part)%') \
+AND NOT (NAME LIKE '%CDP%') AND POP100 > 100000")
+
+geodatahead2.show()
+
 #----------------------------------------------------------------------------------------------------
 print("--- %s seconds ---" % round((time.time() - start_time),4))
 #====================================================================================================
